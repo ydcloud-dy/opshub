4e514fd5b99d167da016b5ac814fc2e850e6e747
J <871578547@qq.com>
2026-01-27 12:38:09 +0800

    feat: 使用shell脚本包装SQL初始化解决权限问题

---
+++ migrations/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# MySQL initialization wrapper script
+# This script ensures proper execution of init.sql
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /docker-entrypoint-initdb.d/init.sql
